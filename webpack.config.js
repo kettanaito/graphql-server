c6ed4a5d9ddf80a011db99ab9df8e62e2f56aed6
Artem Zakharchenko <kettanaito@gmail.com>
2019-01-28 21:21:59 +0100

    Updates to babel@7

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,30 @@
-import path from 'path'
-import webpack from 'webpack'
-import nodeExternals from 'webpack-node-externals'
-import StartServerPlugin from 'start-server-webpack-plugin'
-import packageJson from './package.json'
+const path = require('path')
+const webpack = require('webpack')
+const nodeExternals = require('webpack-node-externals')
+const StartServerPlugin = require('start-server-webpack-plugin')
+const packageJson = require('./package.json')
 
-const DEVELOPMENT = process.env.NODE_ENV === 'development'
+const { NODE_ENV: nodeEnv } = process.env
+const DEVELOPMENT = nodeEnv === 'development'
 
-export default {
+module.exports = {
   target: 'node',
+  mode: nodeEnv,
   entry: {
     index: [
       DEVELOPMENT && 'webpack/hot/signal',
       'regenerator-runtime/runtime',
-      path.resolve(__dirname, packageJson.source)
-    ].filter(Boolean)
+      path.resolve(__dirname, packageJson.source),
+    ].filter(Boolean),
   },
   externals: [
     nodeExternals({
-      whitelist: ['webpack/hot/signal']
-    })
+      whitelist: ['webpack/hot/signal'],
+    }),
   ],
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    filename: '[name].js',
   },
   module: {
     rules: [
@@ -30,34 +32,34 @@ export default {
         test: /\.js$/,
         include: path.resolve(__dirname, 'source'),
         exclude: /node_modules/,
-        use: ['babel-loader']
+        use: ['babel-loader'],
       },
       {
         test: /\.gql$/,
         include: path.resolve(__dirname, 'source'),
         exclude: /node_modules/,
-        use: ['graphql-tag/loader']
-      }
-    ]
+        use: ['graphql-tag/loader'],
+      },
+    ],
   },
   plugins: [
     new webpack.EnvironmentPlugin({
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
     }),
 
     DEVELOPMENT &&
       new StartServerPlugin({
         name: 'index.js',
-        signal: true
+        signal: true,
       }),
 
     new webpack.NamedModulesPlugin(),
     DEVELOPMENT && new webpack.HotModuleReplacementPlugin(),
 
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
   ].filter(Boolean),
   resolve: {
-    extensions: ['.flow.js', '.js']
+    extensions: ['.flow.js', '.js'],
   },
-  devtool: 'sourcemap'
+  devtool: 'sourcemap',
 }
