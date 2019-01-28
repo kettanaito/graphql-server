ad45612ec83db89969cb7ae5c87f65ad4fd3e498
Artem Zakharchenko <kettanaito@gmail.com>
2019-01-28 21:58:55 +0100

    Manages multiple babel configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,34 @@
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+    '@babel/preset-flow',
+  ],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    '@babel/plugin-proposal-export-default-from',
+    '@babel/plugin-proposal-object-rest-spread',
+    '@babel/plugin-proposal-class-properties',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '~': './source',
+        },
+      },
+    ],
+  ],
+  env: {
+    test: {
+      plugins: ['@babel/plugin-transform-modules-commonjs'],
+    },
+  },
 }
